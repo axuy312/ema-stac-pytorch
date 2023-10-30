74a446b4d6f1d9b53fb1fb74c1351aa339dfc03b
WANG SHENG-HUEI <44252923+axuy312@users.noreply.github.com>
2023-10-31 03:24:17 +0800

    Update summary.py

--- summary.py
+++ summary.py
@@ -1,5 +1,5 @@
 #--------------------------------------------#
-#   该部分代码用于看网络结构
+# 此部分程式碼用於查看網路結構
 #--------------------------------------------#
 import torch
 from thop import clever_format, profile
@@ -18,10 +18,10 @@ if __name__ == "__main__":
     dummy_input     = torch.randn(1, 3, input_shape[0], input_shape[1]).to(device)
     flops, params   = profile(model.to(device), (dummy_input, ), verbose=False)
     #--------------------------------------------------------#
-    #   flops * 2是因为profile没有将卷积作为两个operations
-    #   有些论文将卷积算乘法、加法两个operations。此时乘2
-    #   有些论文只考虑乘法的运算次数，忽略加法。此时不乘2
-    #   本代码选择乘2，参考YOLOX。
+    # flops * 2是因為profile沒有將波形當作兩個操作
+    # 有些論文將波形算乘法、加法兩種運算。此時乘以2
+    # 有些論文只考慮乘法的運算次數，忽略加法。此時不乘2
+    # 本代碼選擇乘2，參考YOLOX。
     #--------------------------------------------------------#
     flops           = flops * 2
     flops, params   = clever_format([flops, params], "%.3f")
